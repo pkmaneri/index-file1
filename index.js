cb566976fe672a7d1bd35916d31c36fd99fdff47
Your Name <you@example.com>
2021-01-16 14:30:11 +0530

    fc

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function sum() {
-    const firstNum1 = document.getElementById("firstNum1").value;
+    const firstnum1 = document.getElementById("firstnum1").value;
     const secondNum2 = document.getElementById("secondNum2").value;
-    const sum = parseInt(firstNum1) + parseInt(secondNum2);
+    const sum = parseInt(firstnum1) + parseInt(secondNum2);
     document.getElementById("result").innerHTML = sum;
     console.log(sum)
 }
 function multiple() {
-    const Num1 = document.getElementById("Num1").value;
-    const Num2 = document.getElementById("Num2").value;
-    const multiple = parseInt(Num1) * parseInt(Num2);
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const multiple = parseInt(num1) * parseInt(num2);
     document.getElementById("resultmul").innerHTML = multiple;
     console.log(multiple)
 }
 
 function bmi() {
-    const Mass = document.getElementById("Mass").value;
-    const Height = document.getElementById("Height").value;
-    document.getElementById("resultbmi").innerHTML = parseInt(Mass) / (parseInt(Height * Height))
-    console.log((parseInt(Height * Height)))
+    const mass = document.getElementById("mass").value;
+    const height = document.getElementById("height").value;
+    document.getElementById("resultbmi").innerHTML = parseInt(mass) / (parseInt(height * height))
+    console.log(parseInt(mass) / (parseInt(height * height)))
 }
 
