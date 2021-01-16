34cb0e959e176a4724787a0bd7c71a46a336d4c4
Your Name <you@example.com>
2021-01-16 13:51:38 +0530

    multiple add

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function sum() {
-    let firstNum1 = document.getElementById("firstNum1").value;
-    let secondNum2 = document.getElementById("secondNum2").value;
-    let  sum = parseInt(firstNum1) + parseInt(secondNum2)
+    const firstNum1 = document.getElementById("firstNum1").value;
+    const secondNum2 = document.getElementById("secondNum2").value;
+    const  sum = parseInt(firstNum1) + parseInt(secondNum2)
     document.getElementById("result").innerHTML = sum
     console.log(sum)
+}
+function multiple() {
+    const Num1 = document.getElementById("Num1").value;
+    const Num2 = document.getElementById("Num2").value;
+    const  multiple = parseInt(Num1) * parseInt(Num2)
+    document.getElementById("result").innerHTML = multiple
+    console.log(multiple)
 }
 
