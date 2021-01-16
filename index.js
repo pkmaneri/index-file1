9b886275cdd3d2c24300bb3218b65ac8518e2abc
Your Name <you@example.com>
2021-01-16 13:28:07 +0530

    parseInt add in number

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function sum() {
-    let a = document.getElementById("firstNum1").value;
-    let b = document.getElementById("secondNum2").value;
-
-    let c = document.getElementById("result").innerHTML
-    console.log(c)
+    let firstNum1 = document.getElementById("firstNum1").value;
+    let secondNum2 = document.getElementById("secondNum2").value;
+    let  sum = parseInt(firstNum1) + parseInt(secondNum2)
+    document.getElementById("result").innerHTML = sum
+    console.log(sum)
 }
 
