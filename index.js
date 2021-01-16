4c0500d48d06eb60dff6f4424d5626467c1cea87
Your Name <you@example.com>
2021-01-16 13:10:18 +0530

    js file change

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function sum(a,b){
-    // if (typeof a == "firstnum1" && typeof b == "secondnum2")
-    // return (a + b);
-    // else
-    // ("a+b")
+function sum() {
+    let a = document.getElementById("firstNum1").value;
+    let b = document.getElementById("secondNum2").value;
 
-    
-    }
-    
-   
+    let sum = document.getElementById("result").innerHTML
+    console.log(sum)
+}
+
