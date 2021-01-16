9be198c068b67cac20de210d88552c0416bfaaf9
Your Name <you@example.com>
2021-01-16 13:13:31 +0530

    fc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sum() {
     let a = document.getElementById("firstNum1").value;
     let b = document.getElementById("secondNum2").value;
 
-    let sum = document.getElementById("result").innerHTML
-    console.log(sum)
+    let c = document.getElementById("result").innerHTML
+    console.log(c)
 }
 
